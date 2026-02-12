4f98a4b233bdfb1643845e146250b717f821eda5
kssim <kssim@inzisoft.com>
2026-02-12 17:07:24 +0900

    chore: initialize project

---
+++ build.gradle.kts
@@ -0,0 +1,74 @@
+plugins {
+    java
+    alias(libs.plugins.spring.boot)
+    alias(libs.plugins.spring.dependency.management)
+    id("jacoco")
+}
+
+configurations {
+    compileOnly {
+        extendsFrom(configurations.annotationProcessor.get())
+    }
+}
+
+allprojects {
+    group = property("app.group").toString()
+}
+
+dependencyManagement {
+    imports {
+        mavenBom(libs.spring.cloud.dependencies.get().toString())
+    }
+}
+
+dependencies {
+    implementation(libs.spring.boot.starter.web)
+    implementation(libs.spring.boot.starter.validation)
+    implementation(libs.spring.boot.starter.aop)
+    implementation(libs.spring.boot.starter.jpa)
+    implementation(libs.spring.boot.starter.redisson)
+    implementation(libs.mysql.connector)
+    implementation("org.springframework.retry:spring-retry")
+    implementation("org.springframework.boot:spring-boot-starter-cache")
+
+    // Kafka
+    implementation("org.springframework.kafka:spring-kafka")
+
+    // Swagger/OpenAPI Documentation
+    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.3.0")
+
+    compileOnly(libs.lombok)
+    annotationProcessor(libs.lombok)
+
+    // 테스트 코드에서만 사용한다면, testCompileOnly/testAnnotationProcessor로 분리 가능
+    testCompileOnly("org.projectlombok:lombok")
+    testAnnotationProcessor("org.projectlombok:lombok")
+    annotationProcessor(libs.spring.boot.configuration.processor)
+    testImplementation(libs.bundles.testcontainers.mysql)
+    //testImplementation(libs.test.containers.kafka)
+    testImplementation("org.awaitility:awaitility:4.2.0")
+    implementation("p6spy:p6spy:3.9.1")
+    implementation("com.github.gavlyukovskiy:datasource-decorator-spring-boot-autoconfigure:1.9.2")
+}
+
+// about source and compilation
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+}
+
+with(extensions.getByType(JacocoPluginExtension::class.java)) {
+    toolVersion = "0.8.7"
+}
+
+// bundling tasks
+tasks.getByName("bootJar") {
+    enabled = true
+}
+tasks.getByName("jar") {
+    enabled = false
+}
+// test tasks
+tasks.test {
+    ignoreFailures = true
+    useJUnitPlatform()
+}
